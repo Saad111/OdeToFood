21c44b8f7ed692717b589f7301bba1f95c752d08
Syed Saad Ali <saad_xmart@hotmail.com>
2019-10-28 11:58:41 -0400

    Initial Commit
    Adding to GtiHub Repository

---
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Data;
+using OdeToFood.Core;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
+
+        //Properties that View can Bind to
+        //When the user clicks Save button, this Restaurant should be populated with the information on the form
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+        //Class constructor with data service IRestaurant and we call this restaurantData and save this in private field restaurantData
+        public EditModel(IRestaurantData restaurantData,
+                          IHtmlHelper htmlHelper)
+        {
+            this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+            Restaurant = restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+            restaurantData.Update(Restaurant);
+            restaurantData.Commit();
+            return Page();
+        }
+    }
+}
